081825c1683c0a1c9fa999b1782d50a48b141b6f
tj on laptop <thomasjnix.dev@gmail.com>
2021-10-25 00:50:24 -0400

    Default value caeser cipher

--- caeser_cipher.js
+++ caeser_cipher.js
@@ -1,4 +1,4 @@
-const getCaeserCipher = (phrase, shiftAmount) => {
+const getCaeserCipher = (phrase, shiftAmount = 1) => {
     const alphabet = [
         ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
         ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]];
